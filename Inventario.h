f3b72a85aff131426b541e1dac50a4b6f40e1fb0
SaraGonzalez-07 <sara.michelle.gonzalez@correounivalle.edu.co>
2025-11-13 20:40:23 -0500

    Update Inventario.h

--- Inventario.h
+++ Inventario.h
@@ -3,7 +3,8 @@
  * @brief Definición de la interfaz de la clase Inventario.
  * @details Este archivo contiene la declaración de la clase Inventario, que se encarga
  * de almacenar y gestionar la colección de objetos Ingrediente, incluyendo
- * funcionalidades de CRUD, búsqueda y persistencia en archivos.
+ * funcionalidades de manipulación (adición, eliminación, modificación y consulta),
+ * búsqueda y persistencia en archivos.
  * @author [Sara Gonzalez, Evan Arregoces, Lissette Patiño, Margareth Gamboa, Luvian Parra]
  * @mail [sara.michelle.gonzalez@correounivalle.edu.co, evan.arregoces@correounivalle.edu.co, lissette.patino@correounivalle.edu.co, margareth.gamboa@correounivalle.edu.co, luvian.parra@correounivalle.edu.co]
  * @date 2025-11-13
