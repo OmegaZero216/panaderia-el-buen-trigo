01cc87b24fd0decc92d3cfb8b319c46136c05fd0
SaraGonzalez-07 <sara.michelle.gonzalez@correounivalle.edu.co>
2025-11-13 19:24:04 -0500

    Update GestorInventario.h

--- GestorInventario.h
+++ GestorInventario.h
@@ -1,3 +1,15 @@
+/**
+ * @file GestorInventario.h
+ * @brief Definición de la interfaz de la clase GestorInventario.
+ * @details Este archivo contiene la declaración de la clase GestorInventario, que actúa
+ * como un intermediario para todas las operaciones de modificación y consulta
+ * del Inventario, así como para la consulta del Stock.
+ * @author [Sara Gonzalez, Evan Arregoces, Lissette Patiño, Margareth Gamboa, Luvian Parra]
+ * @mail [sara.michelle.gonzalez@correounivalle.edu.co, evan.arregoces@correounivalle.edu.co, lissette.patino@correounivalle.edu.co, margareth.gamboa@correounivalle.edu.co, luvian.parra@correounivalle.edu.co]
+ * @date 2025-11-13
+ * @version 2.0
+ */
+
 #ifndef GESTOR_INVENTARIO_H
 #define GESTOR_INVENTARIO_H
 
