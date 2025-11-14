db48d0a3621034b4f3536fd3d0df3290357e33cd
SaraGonzalez-07 <sara.michelle.gonzalez@correounivalle.edu.co>
2025-11-13 19:15:32 -0500

    Update Administrador.cpp

--- Administrador.cpp
+++ Administrador.cpp
@@ -1,3 +1,15 @@
+/**
+ * @file Administrador.cpp
+ * @brief Implementación de la clase Administrador.
+ * @details Este archivo contiene las definiciones de los métodos de la clase Administrador,
+ * utilizados para generar diversos reportes sobre el estado del Inventario y el Stock
+ * (Reporte de inventario, Reporte de stock, Reporte porcentual y Reporte de cierre anual).
+ * @author [Sara Gonzalez, Evan Arregoces, Lissette Patiño, Margareth Gamboa, Luvian Parra]
+ * @mail [sara.michelle.gonzalez@correounivalle.edu.co, evan.arregoces@correounivalle.edu.co, lissette.patino@correounivalle.edu.co, margareth.gamboa@correounivalle.edu.co, luvian.parra@correounivalle.edu.co]
+ * @date 2025-11-13
+ * @version 2.0
+ */
+
 #include "Administrador.h"
 #include <iostream>
 #include <iomanip>
