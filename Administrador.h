33013860de41709a1ed7bf4cb700d93b59a6563c
SaraGonzalez-07 <sara.michelle.gonzalez@correounivalle.edu.co>
2025-11-13 19:19:07 -0500

    Update Administrador.h

--- Administrador.h
+++ Administrador.h
@@ -1,3 +1,14 @@
+/**
+ * @file Administrador.h
+ * @brief Definición de la interfaz de la clase Administrador.
+ * @details Este archivo contiene la declaración de la clase Administrador, que gestiona
+ * la lógica para la generación de reportes del sistema de inventario y stock.
+ * @author [Sara Gonzalez, Evan Arregoces, Lissette Patiño, Margareth Gamboa, Luvian Parra]
+ * @mail [sara.michelle.gonzalez@correounivalle.edu.co, evan.arregoces@correounivalle.edu.co, lissette.patino@correounivalle.edu.co, margareth.gamboa@correounivalle.edu.co, luvian.parra@correounivalle.edu.co]
+ * @date 2025-11-13
+ * @version 2.0
+ */
+
 #ifndef ADMINISTRADOR_H
 #define ADMINISTRADOR_H
 
