c021f99f3b458af71f962e86fa4cf5e882998e5a
SaraGonzalez-07 <sara.michelle.gonzalez@correounivalle.edu.co>
2025-11-13 19:50:02 -0500

    Update Panadero.h

--- Panadero.h
+++ Panadero.h
@@ -1,3 +1,15 @@
+/**
+ * @file Panadero.h
+ * @brief Definición de la interfaz de la clase Panadero.
+ * @details Este archivo contiene la declaración de la clase Panadero, que actúa
+ * como el gestor principal de las recetas y el inicio de los procesos
+ * de producción, interactuando con las clases Receta y Produccion.
+ * @author [Sara Gonzalez, Evan Arregoces, Lissette Patiño, Margareth Gamboa, Luvian Parra]
+ * @mail [sara.michelle.gonzalez@correounivalle.edu.co, evan.arregoces@correounivalle.edu.co, lissette.patino@correounivalle.edu.co, margareth.gamboa@correounivalle.edu.co, luvian.parra@correounivalle.edu.co]
+ * @date 2025-11-13
+ * @version 1.0
+ */
+
 #ifndef PANADERO_H
 #define PANADERO_H
 
